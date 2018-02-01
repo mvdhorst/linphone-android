42e0f49e7131564aa0c0b312c429cbbeafe1d4da
Michael van der Horst <mvdhorst@gmail.com>
2018-02-01 15:09:29 +0100

    Add hangUp uri to hangup selected call

--- src/android/org/linphone/HangupReceiver.java
+++ src/android/org/linphone/HangupReceiver.java
@@ -7,11 +7,13 @@ import android.os.Bundle;
 
 import org.linphone.core.LinphoneCall;
 import org.linphone.core.LinphoneCore;
-import org.linphone.mediastream.Log;
 
 /**
  * Created by mvdhorst on 18-12-17.
  * Hangs up a call.
+ *
+ * 01-02-18 rvdillen Add hangUp uri to hangup selected call
+ * 18-12-17 mvdhorst Initial version
  */
 
 public class HangupReceiver extends BroadcastReceiver {
@@ -19,9 +21,39 @@ public class HangupReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         if(isOrderedBroadcast())
             abortBroadcast();
+
         LinphoneCore lc = LinphoneManager.getLc();
         LinphoneCall currentCall = lc.getCurrentCall();
 
+        // Find Sip Nr to hang up
+        String hangUpNr = null;
+        if (intent != null) {
+            Bundle extras = intent.getExtras();
+            if (extras != null && extras.containsKey("uri")) {
+                hangUpNr = extras.getString("uri");
+            }
+        }
+
+        // Find Sip Nr of current call
+        String contact = null;
+        if (currentCall != null) {
+            contact = FormatUri(currentCall.getRemoteContact());
+        }
+
+        // If HangUpNr added and not Equals current call, do not hang up current call, try terminate from list
+        if (contact != null && hangUpNr != null && !contact.equalsIgnoreCase(hangUpNr))  {
+            LinphoneCall[] calls= lc.getCalls();
+            for (LinphoneCall call : calls) {
+                String uri= FormatUri(call.getRemoteContact());
+                if (uri != null && uri.equalsIgnoreCase(hangUpNr)) {
+                    lc.terminateCall(call);
+                }
+            }
+            return;
+        }
+        // <= Validation on sip HangUp nr
+
+        // Default hangup behaviour
         if (currentCall != null) {
             lc.terminateCall(currentCall);
         } else if (lc.isInConference()) {
@@ -29,5 +61,16 @@ public class HangupReceiver extends BroadcastReceiver {
         } else {
             lc.terminateAllCalls();
         }
+    }
+
+    private String FormatUri (String uri){
+
+        if (uri != null) {
+            int index = uri.indexOf("@");
+            if (index > -1){
+                uri = uri.substring(0, index);
+            }
+        }
+        return uri;
     }
 }
